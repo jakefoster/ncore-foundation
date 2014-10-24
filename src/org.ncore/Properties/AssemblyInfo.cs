7dbf0c89b6e73b2ff664d64d7c47dcf6c8873c0a
Jake Foster <jake@jakefoster.com>
2014-10-23 22:30:46 -0700

    New build and NuGet package.

--- src/org.ncore/Properties/AssemblyInfo.cs
+++ src/org.ncore/Properties/AssemblyInfo.cs
@@ -62,7 +62,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.7.5.241" )]
-[assembly: AssemblyFileVersion( "1.7.5.241" )]
+[assembly: AssemblyVersion( "1.7.5.242" )]
+[assembly: AssemblyFileVersion( "1.7.5.242" )]
 
 
